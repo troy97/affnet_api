3fb15dff87e89d23e9a0726ae4e1c9241b72513a
a.lukashchuk <a.lukashchuk@e5628cc2-a3fe-421b-95b9-523c3cd25882>
2014-09-25 09:02:38 +0000

    sun http server + upload

--- trunk/src/main/java/eu/ibutler/affiliatenetwork/dao/UserDao.java
+++ trunk/src/main/java/eu/ibutler/affiliatenetwork/dao/UserDao.java
@@ -1,5 +1,27 @@
 package eu.ibutler.affiliatenetwork.dao;
 
+import java.util.List;
+
+import eu.ibutler.affiliatenetwork.dao.exceptions.DbAccessException;
+import eu.ibutler.affiliatenetwork.dao.exceptions.NoSuchUserException;
+import eu.ibutler.affiliatenetwork.entity.User;
+
 public interface UserDao {
+	
+	/**
+	 * check if there's such user in the DB
+	 * @param login
+	 * @param password
+	 * @return User object or "null" if there's no such user
+	 */
+	public User login(String login, String password) throws DbAccessException, NoSuchUserException;
+
+	/**
+	 * Adds new user to DB
+	 * @param user
+	 * @return id of new user in the DB
+	 */
+	public List<User> getAllUsers() throws DbAccessException;
+
 
 }
