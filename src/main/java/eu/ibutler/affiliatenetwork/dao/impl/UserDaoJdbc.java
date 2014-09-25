58923e77bae3b56074534566022b39f818caa5fb
a.lukashchuk <a.lukashchuk@e5628cc2-a3fe-421b-95b9-523c3cd25882>
2014-09-25 09:14:46 +0000

    Sun http + upload

--- src/main/java/eu/ibutler/affiliatenetwork/dao/impl/UserDaoJdbc.java
+++
@@ -1,7 +0,0 @@
-package eu.ibutler.affiliatenetwork.dao.impl;
-
-import eu.ibutler.affiliatenetwork.dao.UserDao;
-
-public class UserDaoJdbc implements UserDao {
-
-}
--- src/main/resources/db/migration/V1_1__populate_db.sql
+++
@@ -1,5 +0,0 @@
-insert into tbl_users
-values(1, 'ebay', 'ebay', '1111', 'ebay@gmail.com');
-
-insert into tbl_users
-values(2, 'amazon', 'amazon', '1111', 'amazon@gmail.com');
