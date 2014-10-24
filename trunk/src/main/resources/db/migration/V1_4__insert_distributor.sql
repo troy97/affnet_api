70f713258b452ef720583ba61e5a4d61dee275f4
a.lukashchuk <a.lukashchuk@e5628cc2-a3fe-421b-95b9-523c3cd25882>
2014-10-24 12:19:24 +0000

    Changed DB credentials in pom.xml, added new migrations

---
+++ trunk/src/main/resources/db/migration/V1_4__insert_distributor.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.tbl_distributors (email, name_first, name_last, password_ssha256_hex)
+ VALUES ( 'distrib@distrib.net', 'Distributor', 'Distributor', '9860c5afc4e0b5621052ea044d6c8bb1ee1f56bee662e2d0766b44bfef67ba46');
