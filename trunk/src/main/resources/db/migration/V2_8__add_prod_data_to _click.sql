d271e8d1d399f585839bc3eb810f37ce774b3645
a.lukashchuk <a.lukashchuk@e5628cc2-a3fe-421b-95b9-523c3cd25882>
2014-11-13 14:21:19 +0000

    Changed v2.8 migration

--- trunk/src/main/resources/db/migration/V2_8__add_prod_data_to _click.sql
+++ trunk/src/main/resources/db/migration/V2_8__add_prod_data_to _click.sql
@@ -2,11 +2,13 @@ BEGIN;
 
 
 -- CREATE FIELD "product_name" ---------------------------------
-ALTER TABLE "public"."tbl_clicks" ADD COLUMN "product_name" Character Varying( 256 ) NOT NULL;
+ALTER TABLE "public"."tbl_clicks" ADD COLUMN "product_name" Character Varying( 256 ) NOT NULL DEFAULT 'foo';
+ALTER TABLE "public"."tbl_clicks" ALTER COLUMN "product_name" DROP DEFAULT;
 -- -------------------------------------------------------------
 
 -- CREATE FIELD "product_price" --------------------------------
-ALTER TABLE "public"."tbl_clicks" ADD COLUMN "product_price" Double Precision NOT NULL;
+ALTER TABLE "public"."tbl_clicks" ADD COLUMN "product_price" Double Precision NOT NULL DEFAULT -1.0;
+ALTER TABLE "public"."tbl_clicks" ALTER COLUMN "product_price" DROP DEFAULT;
 -- -------------------------------------------------------------
 
 -- CREATE FIELD "shipping_price" -------------------------------
